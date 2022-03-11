d6f11a5da138c448f2a7793b99780e32c8b6996c
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-10 23:36:06 -0300

    card , listagem modal image e redirect imgbb

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,24 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+
+  
+
+  return (
+  <Modal isOpen={isOpen} onClose={onClose} >
+     <ModalOverlay />
+        <ModalContent  maxW={'900px'} maxH={'600px'}>
+          <ModalBody justifyContent={"center"} sx={{padding: '0px'}} >
+            <Image src={imgUrl} minWidht={'600px'} maxW={'900px'} maxH={'600px'}/>
+          </ModalBody>
+
+          <ModalFooter backgroundColor={"pGray.800"} justifyContent={"left"} padding={"4px"}   borderRadius={'0px 0px 6px 6px'}>
+              <a>
+                <Link href={imgUrl} fontFamily={"Roboto"} fontSize={"14px"} marginLeft={"10px"}>  Abrir original</Link>
+              </a>
+            
+          </ModalFooter>
+        </ModalContent>
+  </Modal>
+  )
 }
